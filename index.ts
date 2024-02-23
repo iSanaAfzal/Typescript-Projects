3f17b569023d53bd9ff8ea2e48508ba6c64da520
sanuafzal <sanafzal.0985@gmail.com>
2024-02-23 12:37:06 +0500

    todo

---
+++ index.ts
@@ -0,0 +1,47 @@
+import inquirer from 'inquirer';
+
+async function main() {
+    const todos: string[] = [];
+
+    let loop = true;
+
+    while (loop) {
+        const answers: {
+            TODO: string;
+            retry: boolean;
+        } = await inquirer.prompt([
+            {
+                type: 'input',
+                name: 'TODO',
+                message: 'What do you want to add to your to-do list?',
+            },
+            {
+                type: 'confirm',
+                name: 'retry',
+                message: 'Do you want to add more to-dos?',
+                default: false,
+            },
+        ]);
+
+        const { TODO, retry } = answers;
+
+        if (TODO.trim() !== '') {
+            todos.push(TODO);
+        } else {
+            console.log('Please add a valid input.');
+        }
+
+        loop = retry;
+    }
+
+    if (todos.length > 0) {
+        console.log('\nYour Todo List:\n');
+        todos.forEach((todo, index) => {
+            console.log(`${index + 1}. ${todo}`);
+        });
+    } else {
+        console.log('\nNo Todos Found.');
+    }
+}
+
+main();
